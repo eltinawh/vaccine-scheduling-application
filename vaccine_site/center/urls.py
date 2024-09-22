d0029dc9afad565a4b104838d7d148936bd74392
eltinawh <eltinawh@yahoo.com>
2024-09-22 10:31:08 +0700

    Added Storage CRUD using generic class based view

--- vaccine_site/center/urls.py
+++ vaccine_site/center/urls.py
@@ -8,5 +8,10 @@ urlpatterns = [
     path("<int:id>", views.center_detail, name="detail"),
     path("create/", views.create_center, name="create"),
     path("update/<int:id>", views.update_center, name="update"),
-    path("delete/<int:id>",views.delete_center, name="delete")
+    path("delete/<int:id>",views.delete_center, name="delete"),
+    path("<int:center_id>/storage/", views.StorageList.as_view(), name="storage-list"),
+    path("storage/<int:pk>/", views.StorageDetail.as_view(), name="storage-detail"),
+    path("<int:center_id>/storage/create/", views.CreateStorage.as_view(), name="create-storage"),
+    path("storage/update/<int:pk>/", views.UpdateStorage.as_view(), name="update-storage"),
+    path("storage/delete/<int:pk>/", views.DeleteStorage.as_view(), name="delete-storage"),
 ]
--- vaccine_site/center/views.py
+++ vaccine_site/center/views.py
@@ -1,22 +1,24 @@
+from typing import Any
 from django.shortcuts import render, redirect
-from center.models import Center
-from center.forms import CenterForm
+from center.models import Center, Storage
+from center.forms import CenterForm, StorageForm
 from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
+from django.views import generic
 
 def center_list(request):
     objects = Center.objects.all()
     context = {
         "center": objects,
     }
-    return render(request, "center-list.html", context)
+    return render(request, "center/center-list.html", context)
 
 def center_detail(request, id):
     center = Center.objects.get(id=id)
     context = {
         'center': center
     }
-    return render(request, "center-detail.html", context)
+    return render(request, "center/center-detail.html", context)
 
 def create_center(request):
     if request.method == "POST":
@@ -24,12 +26,12 @@ def create_center(request):
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse("center:list"))
-        return render(request, "create-center.html", {"form": form})
+        return render(request, "center/create-center.html", {"form": form})
     # GET
     context = {
         "form": CenterForm()
     }
-    return render(request, "create-center.html", context)
+    return render(request, "center/create-center.html", context)
 
 def update_center(request, id):
     try:
@@ -42,12 +44,12 @@ def update_center(request, id):
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse("center:detail", kwargs={"id": center.id}))
-        return render(request, "update-center.html", {"form": form})
+        return render(request, "center/update-center.html", {"form": form})
     # GET
     context = {
         "form": CenterForm(instance=center)
     }
-    return render(request, "update-center.html", context)
+    return render(request, "center/update-center.html", context)
 
 def delete_center(request, id):
     try:
@@ -62,4 +64,67 @@ def delete_center(request, id):
     context = {
         "center": center,
     }
-    return render(request, "delete-center.html", context)
+    return render(request, "center/delete-center.html", context)
+
+class StorageList(generic.ListView):
+    queryset = Storage.objects.all()
+    template_name = "storage/storage-list.html"
+    
+    def get_queryset(self):
+        return super().get_queryset().filter(center_id=self.kwargs["center_id"])
+    
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["center_id"] = self.kwargs["center_id"]
+        return context
+
+    
+class StorageDetail(generic.DetailView):
+    model = Storage
+    template_name = "storage/storage-detail.html"
+    
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["available_quantity"] = self.object.total_quantity - self.object.booked_quantity
+        return context
+    
+
+class CreateStorage(generic.CreateView):
+    model = Storage
+    form_class = StorageForm
+    template_name = "storage/create-storage.html"
+    
+    def get_form_kwargs(self) -> dict[str, Any]:
+        kwargs = super().get_form_kwargs()
+        kwargs["center_id"] = self.kwargs["center_id"]
+        return kwargs
+    
+    def get_initial(self) -> dict[str, Any]:
+        initials = super().get_initial()
+        initials["center"] = Center.objects.get(id=self.kwargs["center_id"])
+        return initials
+    
+    def get_success_url(self) -> str:
+        return reverse("center:storage-list", kwargs={"center_id": self.kwargs["center_id"]})
+    
+
+class UpdateStorage(generic.UpdateView):
+    model = Storage
+    form_class = StorageForm
+    template_name = "storage/update-storage.html"
+    
+    def get_form_kwargs(self) -> dict[str, Any]:
+        kwargs = super().get_form_kwargs()
+        kwargs["center_id"] = self.get_object().center.id
+        return kwargs
+    
+    def get_success_url(self) -> str:
+        return reverse("center:storage-list", kwargs={"center_id": self.get_object().center.id})
+
+
+class DeleteStorage(generic.DeleteView):
+    model = Storage
+    template_name = "storage/delete-storage.html"
+    
+    def get_success_url(self):
+        return reverse("center:storage-list", kwargs={"center_id": self.get_object().center.id})
