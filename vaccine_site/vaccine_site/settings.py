37d8e8a9794b829cdca7cc63235ce23f26aa0b96
eltinawh <eltinawh@yahoo.com>
2024-09-24 15:42:24 +0700

    Added user authentication system

--- vaccine_site/vaccine_site/settings.py
+++ vaccine_site/vaccine_site/settings.py
@@ -124,7 +124,14 @@ STATIC_URL = 'static/'
 STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'static')]
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "user.User"
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
--- vaccine_site/vaccine_site/urls.py
+++ vaccine_site/vaccine_site/urls.py
@@ -19,12 +19,31 @@ from django.urls import path, include
 from vaccine_site import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name="index"),
     path('admin/', admin.site.urls),
     path('center/', include('center.urls', namespace='center')),
     path('vaccine/', include('vaccine.urls', namespace='vaccine')),
+    path('accounts/', include('user.urls', namespace='user')),
+    path("password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
